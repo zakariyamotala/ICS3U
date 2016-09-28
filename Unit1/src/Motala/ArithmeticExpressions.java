4a2e8fc193568570808266b7fadbd9a3c0df571c
323397539 <323397539@C-3ZYDYR1.tdsb.on.ca>
2016-09-28 12:55:38 -0400

    arithmetic expressions #1-5

---
+++ Unit1/src/Motala/ArithmeticExpressions.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package Motala;
+
+import java.util.Scanner;
+/**
+ * @author zakariya
+ *
+ */
+public class ArithmeticExpressions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		 Scanner scan = new Scanner(System.in);
+		 int num1, num2, num3, num4, num5;
+		
+
+		 System.out.println("Enter 5 numbers");
+		 num1=scan.nextInt();
+		 num2=scan.nextInt();
+		 num3=scan.nextInt();
+		 num4=scan.nextInt();
+		 num5=scan.nextInt();
+		 
+		 System.out.println("The total is: " +(num1+num2+num3+num4+num5));
+		 
+		 System.out.println("The result after subtracting the second number from the third is:" +(num2-num3));
+		 
+		 System.out.println("The Product of the first and fifth number is:" +(num1 * num5));
+		 
+		 System.out.println("The quotient of the fourth number divided by the second:" +(num2/num4));
+		 
+		 System.out.println("The remainder when dividing the fourth number by the second:" +(num2 % num4));
+
+		 
+	
+	}	
+	
+	
+	
+
+}
