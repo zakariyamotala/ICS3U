d41f7dc06b694d1b55dd5d165e25f8d9c560433a
323397539 <323397539@C-3ZYDYR1.tdsb.on.ca>
2016-10-04 13:18:12 -0400

    Quadratic formula

---
+++ Unit1/src/Motala/quadraticFormula.java
@@ -0,0 +1,41 @@
+
+/**
+ * 
+ */
+package Motala;
+
+import java.util.Scanner;
+/**
+ * @author zakariya
+ *
+ */
+public class quadraticFormula {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int a,b,c;
+		
+		
+		System.out.println("Enter number in the variables");
+		a=scan.nextInt();
+		b=scan.nextInt();
+		c=scan.nextInt();
+		
+		System.out.println(-b + (Math.sqrt(Math.pow(b,2))+(-4*a*c/2*a)));
+		System.out.println(-b -Math.sqrt(Math.pow(b,2))-4*a*c/2*a);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
