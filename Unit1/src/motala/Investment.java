29bebaa98d57c02e9bcbbf5011f8d1c96c124878
323397539 <323397539@C-3ZYDYR1.tdsb.on.ca>
2016-11-17 13:49:22 -0500

    investment project

---
+++ Unit1/src/motala/Investment.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package motala;
+
+import java.util.Scanner;
+
+/**
+ * @author 323397539
+ *
+ */
+public class Investment {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int investment = 2500;
+		int wInvest = 5000;
+		
+		double cInvest = investment;
+		double years = 1;
+		
+		while (cInvest <= investment){
+			years = years +1;
+			cInvest = cInvest * 1.075;
+		}
+		
+		System.out.println("It will take: " + years + " years, if compounded annualy at 7.5%");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
