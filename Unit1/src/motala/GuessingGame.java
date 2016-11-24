e7044a067210217593cf8e9de7b5efeccbae37b4
323397539 <323397539@C-3ZYDYR1.tdsb.on.ca>
2016-11-24 13:55:46 -0500

    Second part of guessing game

--- Unit1/src/motala/GuessingGame.java
+++ Unit1/src/motala/GuessingGame.java
@@ -6,12 +6,12 @@ package motala;
 import java.util.Scanner;
 
 /**ICS3U
- * @author zakariya
+ * @author zakariya motala
  *November 15,2016
  */
 public class GuessingGame {
 
-	/**If users number is greater than computers number then user wins, if not then user loses
+	/**If users number is greater than computers number, then user wins, if not then user loses
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -20,29 +20,42 @@ public class GuessingGame {
 		
 
 		int usernum;
-		int compnum;
-		
-		
-		System.out.println("Enter a number between 1 and 20: ");
-		usernum = scan.nextInt();
-		
-		if (usernum>0 || usernum<20){
+		int compnum = 0;
+		String tryagain;
+
+
+		do{
+			System.out.println("Enter a number between 1 and 20: ");
+			usernum = scan.nextInt();
+			tryagain=scan.nextLine();
+
+			if (usernum>0 && usernum<20){
+
+				compnum=(int)(Math.random()*20)+1;
+				System.out.println("Computers number: "+ compnum);		
+
+				System.out.println("Player's number: " + usernum);
+			}
+			if (compnum>usernum){
+				System.out.println("You lose, Better luck next time ");
+			}
+			else if (compnum<usernum) {
+				System.out.println("You won!!!");	
+			}
+
+
+			else if(usernum==compnum){
+				System.out.println("Its a draw");
+			}
 			
-		compnum=(int)(Math.random()*20)+1;
-		System.out.println("Computers number: "+ compnum);		
-		
-		System.out.println("Player's number: " + usernum);
-	
-		if (compnum>usernum){
-			System.out.println("Better luck next time =(");
-		}
-		
-		else 
-			System.out.println("You won!!!");
-		}
+			System.out.println("Try again? yes or no?");
+			tryagain=scan.nextLine();
+		} while (tryagain.equals ("yes"));
 		
 		
-
+		
+		
+		
 	}
-
 }
+
