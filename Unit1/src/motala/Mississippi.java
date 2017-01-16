13b6f1976b4af4fe2d6ba9ff59811d05baea3753
323397539 <323397539@C-3ZYDYR1.tdsb.on.ca>
2017-01-16 13:48:14 -0500

    Mississippi program

---
+++ Unit1/src/motala/Mississippi.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package motala;
+
+/**ICS3U
+ * Mississippi.java
+ * @author Zakariya Motala
+ * January 11, 2017 
+ *This program writes out MISSISSIPPI
+ */
+
+
+	public class Mississippi {
+		/**
+		 * print letter M
+		 */
+	public static void M(){
+		System.out.println("M			M");
+		System.out.println("MM         MM");
+		System.out.println("M M	  	  M M");
+		System.out.println("M  M	 M  M");
+		System.out.println("M     M     M");
+		System.out.println("M			M");
+		System.out.println("M			M");
+		System.out.println("M		 	M");
+	}
+	/**
+	 * prints letter I
+	 */
+	public static void I(){
+		System.out.println("IIIIIII");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("IIIIIII");
+	}
+	/**
+	 * prints letter S
+	 */
+	public static void S(){
+
+		System.out.println(" SSSSS");
+		System.out.println("SSS   ");
+		System.out.println("SS    ");
+		System.out.println("SSS   ");
+		System.out.println(" SSSSS");
+		System.out.println("   SSS");
+		System.out.println("  SSS ");
+		System.out.println("SSSS  ");	
+	}
+	/**
+	 * prints letter P
+	 */
+	public static void P(){
+
+		System.out.println("  PPPP ");
+		System.out.println(" P     P");
+		System.out.println(" P     P ");
+		System.out.println(" P     P ");
+		System.out.println(" PPPPPPP ");
+		System.out.println(" P       ");
+		System.out.println(" P       ");
+		System.out.println(" P       ");
+		System.out.println(" P       ");
+
+
+	}
+
+ 
+
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		M();
+		I();
+		S();
+		S();
+		I();
+		S();
+		S();
+		I();
+		P();
+		P();
+		I();
+	}
+
+}
